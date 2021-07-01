52673a04ce1871de3798a99bbdbc9d92cb95571f
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-01 14:09:59 +0900

    Nodejs-Implement 'Not found'

--- main.js
+++ main.js
@@ -4,38 +4,37 @@ var url = require('url');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    var queryData = new URL('http://localhost:3000'+_url).searchParams;
-    var title = queryData.get('id');
-    if(_url == '/'){
-      title='Welcome';
-    }
-    if(_url == '/favicon.ico'){
+    var queryData = new URL('http://localhost:3000'+_url);
+    var pathname = queryData.pathname;
+    var title = queryData.searchParams.get('id');
+
+    if(pathname === '/') {
+      fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+    } else {
       response.writeHead(404);
-      response.end();
-      return;
+      response.end('Not found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', function(err, description) {
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
 });
 app.listen(3000);
