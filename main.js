e91f52cabbc7a9f811e9ad4ff015a0b49f397fc4
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-01 09:31:18 +0900

    Nodejs-Use URL

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    //var queryData = url.parse(_url, true).query;
+    var queryData = new URL('http://localhost:3000'+_url).searchParams;
+    console.log(queryData.get('id'));
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       response.writeHead(404);
       response.end();
       return;
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.get('id'));
 
 });
 app.listen(3000);
