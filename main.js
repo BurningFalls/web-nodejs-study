dabffa01c61cf81c25fb879e64e435c0732712af
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-05 15:11:55 +0900

    Nodejs-Save Edits

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ var app = http.createServer(function(request,response){
           var list = templateList(filelist);
           var template = templateHTML(title, list, `
             <form action="/update_process" method="post">
-            <input type="hidden" name="id" "value=${title}">
+            <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
               <p>
                 <textarea name="description" placeholder="description">${description}</textarea>
@@ -111,6 +111,23 @@ var app = http.createServer(function(request,response){
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
+        })
+      })
+    } else if(pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data) {
+        body = body + data;
+      })
+      request.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error) {
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
         })
       })
     } else {
