031fda5194730ca4b0ad526326f5022ef50382a8
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-05 14:51:37 +0900

    Nodejs-Send information to be modified

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -81,7 +81,7 @@ var app = http.createServer(function(request,response){
       var body = '';
       request.on('data', function(data) {
         body = body + data;
-      });
+      })
       request.on('end', function() {
         var post = qs.parse(body);
         var title = post.title;
@@ -89,6 +89,28 @@ var app = http.createServer(function(request,response){
         fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
           response.writeHead(302, {Location: `/?id=${title}`});
           response.end('success');
+        })
+      })
+    } else if(pathname === '/update') {
+      fs.readdir('./data', function(error, filelist) {
+        var title = queryData.searchParams.get('id');
+        fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `
+            <form action="/update_process" method="post">
+            <input type="hidden" name="id" "value=${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
         })
       })
     } else {
