7792fbd9c86562f168273d4f0969f97f8480f98c
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-05 10:57:08 +0900

    Nodejs-POST

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -56,12 +57,12 @@ var app = http.createServer(function(request,response){
           })
         })
       }
-    } else if (pathname === '/create') {
+    } else if(pathname === '/create') {
       fs.readdir('./data', function(error, filelist) {
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/process_create" method="post">
+          <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -74,7 +75,20 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       })
-    } else {
+    } else if(pathname === '/create_process') {
+      var body = '';
+      request.on('data', function(data) {
+        body = body + data;
+      });
+      request.on('end', function() {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log(post.title);
+      })
+      response.writeHead(200);
+      response.end('success');
+    }  else {
       response.writeHead(404);
       response.end('Not found');
     }
