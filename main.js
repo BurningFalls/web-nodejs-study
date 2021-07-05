c3d1455dac972a0b191b91f02244a7c1b844e23d
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-05 15:36:50 +0900

    Nodejs-Completion of post deletion function

--- main.js
+++ main.js
@@ -135,7 +135,21 @@ var app = http.createServer(function(request,response){
           })
         })
       })
-    } else {
+    } else if(pathname === '/delete_process') {
+      var body = '';
+      request.on('data', function(data) {
+        body = body + data;
+      })
+      request.on('end', function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error) {
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        })
+      })
+    }
+    else {
       response.writeHead(404);
       response.end('Not found');
     }
