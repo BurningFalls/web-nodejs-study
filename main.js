b0a2da231871166e6d4bcd5c96973e24d1995e92
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-08-02 14:57:18 +0900

    Nodejs-Security for output information

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
+const sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -25,12 +26,14 @@ var app = http.createServer(function(request,response){
           var filteredId = path.parse(queryData.searchParams.get('id')).base;
           var title = queryData.searchParams.get('id');
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description);
             var list = template.list(filelist);
-            var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
+            var html = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             `<a href="/create">create</a>
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
             <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
             </form>`);
             response.writeHead(200);
