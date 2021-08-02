8f55158a4b39b4f45240304f284ccb7a7aaac529
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-08-02 11:08:43 +0900

    Nodejs-Security of input information

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -21,8 +22,9 @@ var app = http.createServer(function(request,response){
         })
       } else {
         fs.readdir('./data', function(error, filelist) {
+          var filteredId = path.parse(queryData.searchParams.get('id')).base;
           var title = queryData.searchParams.get('id');
-          fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
             var list = template.list(filelist);
             var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>
@@ -70,8 +72,9 @@ var app = http.createServer(function(request,response){
       })
     } else if(pathname === '/update') {
       fs.readdir('./data', function(error, filelist) {
+        var filteredId = path.parse(queryData.searchParams.get('id')).base;
         var title = queryData.searchParams.get('id');
-        fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
           var list = template.list(filelist);
           var html = template.HTML(title, list, `
             <form action="/update_process" method="post">
@@ -115,7 +118,8 @@ var app = http.createServer(function(request,response){
       request.on('end', function() {
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`, function(error) {
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(error) {
           response.writeHead(302, {Location: `/`});
           response.end();
         })
